39917416635cda5de27d858f007a5f00f0abc3a2
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 14:51:27 +0100

    reverted server js back

--- server.js
+++ server.js
@@ -13,6 +13,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(.listen(process.env.PORT || 8080));
-
-
+app.listen(port);
