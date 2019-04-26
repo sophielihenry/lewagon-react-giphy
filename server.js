920072ead652ba86ef105a9882d8b807343e59d7
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 15:14:54 +0100

    heroku test

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-the __dirname is the current directory from where the script is running
+// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
@@ -14,3 +15,15 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
+
+
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+
+
+// app.use(express.static(__dirname + '/'));
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, './src/index.html'));
+// });
+// app.listen(process.env.PORT || 8080);
