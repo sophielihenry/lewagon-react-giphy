fa77d283211fe9c1bea8d835015edf12dd70868a
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 15:41:50 +0100

    heroku test by adding webpack.prod

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
-
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
+the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
@@ -15,15 +14,3 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-
-
-// var express = require('express');
-// var app = express();
-// var path = require('path');
-
-
-// app.use(express.static(__dirname + '/'));
-// app.get('*', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, './src/index.html'));
-// });
-// app.listen(process.env.PORT || 8080);
