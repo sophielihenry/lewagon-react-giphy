f3392360a0fbf9a4236ba30a98679c36270f9e05
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-04-26 12:13:13 +0100

    trying different solutions

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'templates')));
 
 app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
 app.listen(PORT);
